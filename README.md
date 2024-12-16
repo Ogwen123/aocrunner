a15a87a6c6e6bd894a0c3491411a64adf38e7948
Ogwen123 <owen.d.jones@btinternet.com>
2024-12-16 22:15:37 +0000

    validation

---
+++ README.md
@@ -0,0 +1,9 @@
+# aocrunner
+
+An executable to run and submit advent of code solutions written in python. The output of the python file should be just two numbers for part 1 and part 2.
+## Example
+```shell
+...\2024\6> python3 main.py
+1234
+5678
+```
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,54 @@
 mod utils;
 
-use crate::utils::logger::{fatal, info};
+use crate::utils::logger::{fatal};
 use std::env;
+use std::str::FromStr;
 
-fn run(year: usize, day: usize) {
+type ParseResult = Result<i32, <i32 as FromStr>::Err>;
 
+fn run(year: i32, day: i32) {
+    println!("doing stuff")
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        fatal!("You should provide the year and the day as inputs!")
+    // validate the args
+    if args.len() != 3 { // the command itself is part of the args array
+        fatal!("You should provide the year and the day as inputs.");
+        return;
     }
 
-    for  arg in args{
-        println!("{}", arg)
+    // make sure the 2 provided args are valid e.g. year - 20** and day is between 1 and 25 inclusive
+    let year_result: ParseResult = args[1].parse();
+
+    let year = match year_result {
+        Ok(year) => year,
+        Err(_) => {
+            fatal!("Could not convert the year provided to an int.");
+            return;
+        }
+    };
+
+    let day_result: ParseResult = args[2].parse();
+
+    let day = match day_result {
+        Ok(day) => day,
+        Err(_) => {
+            fatal!("Could not convert the day provided to an int.");
+            return;
+        }
+    };
+
+    if year < 2000 || year > 3000 {
+        fatal!("Invalid year.");
+        return;
     }
+
+    if day < 1 || day > 25 {
+        fatal!("Invalid day.");
+        return;
+    }
+
+    run(year, day);
 }
